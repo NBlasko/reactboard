a2282ad01663a3b4db3c3a62fe991b5bf454e1a3
Nikola <epodos.blasko@gmail.com>
2020-03-15 15:39:41 +0100

    updated all dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ReactBoard from './components/ReactBoard';
 import 'bootswatch/dist/cerulean/bootstrap.min.css'
-import registerServiceWorker from './registerServiceWorker';
-
+//import App from './App';
+import * as serviceWorker from './serviceWorker';
 import store from './store';
 import { Provider } from 'react-redux';
 
 
 
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 ReactDOM.render(
     <Provider store={store}>
         <ReactBoard />
     </Provider>, document.getElementById('root'));
-registerServiceWorker();
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
